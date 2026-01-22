e22631b5944957e6543afe7e26021d50c5b1a0f1
javaExactus <romulo.neves@exactus.com.br>
2026-01-22 19:35:39 -0300

    first commit

---
+++ src/main/java/br/com/romulo/apimedico/ApimedicoApplication.java
@@ -0,0 +1,13 @@
+package br.com.romulo.apimedico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApimedicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApimedicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/romulo/apimedico/ApimedicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.romulo.apimedico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApimedicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
